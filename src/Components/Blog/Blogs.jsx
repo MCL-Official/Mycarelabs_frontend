61e7f0c150bb69e3902f16e0749d0bd591da76a8
Harshkhosla <harshkhosla9945@gmail.com>
2024-08-03 00:55:30 +0530

    working

--- src/Components/Blog/Blogs.jsx
+++ src/Components/Blog/Blogs.jsx
@@ -152,9 +152,9 @@ const Blogs = () => {
                 className="w-full pl-12 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg sm:max-w-xs"
               />
             </div>
-            <button className="block w-full mt-3 py-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 active:shadow-none rounded-lg shadow sm:mt-0 sm:w-auto">
+            {/* <button className="block w-full mt-3 py-3 px-4 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 active:shadow-none rounded-lg shadow sm:mt-0 sm:w-auto">
               Subscribe
-            </button>
+            </button> */}
           </div>
         </div>
 
