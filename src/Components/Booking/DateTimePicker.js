f1bbc0772e819944fab9782f9045e17b329f4fbf
Harshkhosla <harshkhosla9945@gmail.com>
2024-08-08 14:43:23 -0700

    date corrrecting to pst

--- src/Components/Booking/DateTimePicker.js
+++ src/Components/Booking/DateTimePicker.js
@@ -5,6 +5,7 @@ import logo1 from "../../Assets/Logo1.png";
 import { useNavigate } from 'react-router-dom';
 import Example from './BarLoader';  // Ensure correct path
 import StackedNotifications from './StackedNotifications';  // Ensure correct path
+import moment from 'moment-timezone';
 
 const LeftContainer = ({ cardData }) => {
   return (
@@ -76,6 +77,9 @@ const DateTimePicker = (cardData) => {
     instructions: '',
     passportDetails: '',
     foundVia: '',
+    Employee: cardData?.cardData?.category,
+    Location: cardData?.cardData?.location,
+    Service: ''
   });
   const [invalidFields, setInvalidFields] = useState({});
 
@@ -105,8 +109,7 @@ const DateTimePicker = (cardData) => {
   };
 
   const timeSlots = [
-    "9:00 AM",
-    "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM",
+    "9:00 AM", "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM",
     "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM",
     "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM",
     "5:00 PM", "5:30 PM", "6:00 PM"
@@ -156,7 +159,7 @@ const DateTimePicker = (cardData) => {
 
     const appointmentDetails = {
       ...formData,
-      date: selectedDate,
+       date: moment.tz(`${selectedDate} ${selectedTime}`, "America/Los_Angeles").format(),
       time: selectedTime,
     };
 
