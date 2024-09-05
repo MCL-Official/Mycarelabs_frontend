f1ba4c35fc3fc1a8b51a8e8d68fdb7c72e8aac77
Harshkhosla <harshkhosla9945@gmail.com>
2024-09-05 23:52:54 +0530

    uat commit

---
+++ src/Pages/Uta/SubElement2.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import leftImage from '../../Assets/uti2.png'; // Make sure to replace with your actual image path
+
+const SubElement2 = () => {
+    return (
+      <div className="flex flex-col md:flex-row items-center bg-white shadow-lg rounded-lg overflow-hidden">
+        {/* Image Container */}
+        <div className="md:w-1/2">
+          <img src={leftImage} alt="Description" className="w-full h-50 object-contain"/>
+        </div>
+        
+        {/* Text Content Container */}
+        <div className="md:w-1/2 p-4 md:p-8">
+          <h1 className="text-5xl font-bold text-gray-800 mb-2 text-left">What is a UTI?</h1>
+          <p className="text-gray-600 text-lg  text-left pt-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    )
+  }
+
+export default SubElement2;
---
+++ src/Pages/Uta/Uta.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Hero from './Hero'
+import SubElement from './SubElement'
+import SubElement1 from './SubElement1'
+import SubElement2 from './SubElement2'
+import SubElement3 from './SubElement3'
+
+const Uta = () => {
+  return (
+    <div>
+      <br></br>
+      <br></br>
+      <br></br>
+      {/* <br></br> */}
+      <Hero/>
+      <SubElement/>
+      <SubElement1/>
+      <SubElement2/>
+      <SubElement3/>
+
+    </div>
+  )
+}
+
+export default Uta
