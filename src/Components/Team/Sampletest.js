0a154fce90d8c4491ea8a8f6af69027e61fce409
Harshkhosla <harshkhosla9945@gmail.com>
2024-09-17 03:05:30 +0530

    THE chnegbn

--- src/Components/Team/Sampletest.js
+++ src/Components/Team/Sampletest.js
@@ -1,10 +1,10 @@
 import { Helmet } from "react-helmet";
-
+ 
 export function Sampletest () {
-
+ 
     const team = [
         {
-            avatar: "https://i.ibb.co/GPHpyRv/1.png",
+            avatar: "https://i.ibb.co/NC5zjr5/1.png",
             name: "Harvey Singh",
             title: "Chief Executive Officer ",
             desc: "",
@@ -13,7 +13,7 @@ export function Sampletest () {
             github: "javascript:void(0)"
         },
         {
-            avatar: "https://i.ibb.co/Thv7q7k/5.png",
+            avatar: "https://i.ibb.co/YNfcmLh/5.png",
             name: "Aman Singh",
             title: "Vice President, Global Operations",
             desc: "",
@@ -21,17 +21,17 @@ export function Sampletest () {
             twitter: "javascript:void(0)",
             github: "javascript:void(0)"
         },
-        // {
-        //     avatar: "https://mycarelabs.com/wp-content/uploads/2023/11/Ellipse-11.png",
-        //     name: "Dr. Carl Johnson",
-        //     title: "Medical Director",
-        //     desc: "",
-        //     linkedin: "javascript:void(0)",
-        //     twitter: "javascript:void(0)",
-        //     github: "javascript:void(0)"
-        // },
         {
-            avatar: "https://i.ibb.co/YyXtZZd/sdad.png",
+            avatar: "https://i.ibb.co/vQ4c1cD/11.png",
+            name: "Dr. Carl Johnson",
+            title: "Medical Director",
+            desc: "",
+            linkedin: "javascript:void(0)",
+            twitter: "javascript:void(0)",
+            github: "javascript:void(0)"
+        },
+        {
+            avatar: "https://i.ibb.co/QCsCtsb/sdad.png",
             name: "Kishan Singh",
             title: "Vice President, India Operatoins",
             desc: "",
@@ -40,7 +40,7 @@ export function Sampletest () {
             github: "javascript:void(0)"
         },
         {
-            avatar: "https://i.ibb.co/dtVPXMC/2.png",
+            avatar: "https://i.ibb.co/3FCmdS8/2.png",
             name: "Emily Garcia",
             title: "Marketing Manager",
             desc: "",
@@ -49,7 +49,7 @@ export function Sampletest () {
             github: "javascript:void(0)"
         },
         {
-            avatar: "https://i.ibb.co/4pZJvwR/3.png",
+            avatar: "https://i.ibb.co/n8vpYhf/3.png",
             name: "Soun Kanwar",
             title: "Communications Director",
             desc: "",
@@ -58,7 +58,7 @@ export function Sampletest () {
             github: "javascript:void(0)"
         },
         {
-            avatar: "https://i.ibb.co/ZLz23Lq/DFYHF.png",
+            avatar: "https://i.ibb.co/0G8HJ6R/4.png",
             name: "Pramo Virk",
             title: "VP of Europe Operation",
             desc: "",
@@ -67,7 +67,7 @@ export function Sampletest () {
             github: "javascript:void(0)"
         },
         {
-            avatar: "https://i.ibb.co/9qsSKMB/4.png",
+            avatar: "https://i.ibb.co/2qBFfYy/4.png",
             name: "Ralph Giron",
             title: "General Manager, USA",
             desc: "",
@@ -76,11 +76,11 @@ export function Sampletest () {
             github: "javascript:void(0)"
         },
     ]
-
+ 
     return (
-      
+     
         <section className="py-14">
-              
+             
      <Helmet>
         <title>Welcome to My Care Labs | Comprehensive Health Solutions</title>
         <meta name="description" content="Discover comprehensive health solutions at My Care Labs. From infectious diseases to wellness and toxicology, we're here for your well-being." />
@@ -97,7 +97,7 @@ export function Sampletest () {
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since.
     </p> */}
 </div>
-
+ 
                 <div className="mt-12">
                     <ul className="grid gap-8 sm:grid-cols-2 md:grid-cols-4">
                         {
