8df2b5f8dfbf6891f6299bb200874a6d85e371e9
skyi9 <88197965+skyi9@users.noreply.github.com>
2024-12-24 03:23:57 +0530

    btn ids mentioned

--- src/Components/Booking/DateTimePicker.js
+++ src/Components/Booking/DateTimePicker.js
@@ -539,7 +539,7 @@ const DateTimePicker = ({ cardData, CrelioData }) => {
               <div className="text-center text-gray-700 mt-4">
                 {/* Time zone <span className="font-semibold">India Standard Time (7:34pm)</span> */}
               </div>
-              <button className="w-full enabled:bg-blue-500 disabled:bg-gray-500 text-white py-2 rounded mt-4 transition-transform transform hover:scale-105" disabled={selectedTime ? false : true} onClick={handleContinue}>
+              <button id="#ContinueBook" className="w-full enabled:bg-blue-500 disabled:bg-gray-500 text-white py-2 rounded mt-4 transition-transform transform hover:scale-105" disabled={selectedTime ? false : true} onClick={handleContinue}>
                 Continue
               </button>
             </>
