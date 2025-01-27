fbb069f4e7486cc671b9a364dae42474b0603826
skyi9 <88197965+skyi9@users.noreply.github.com>
2025-01-28 03:39:01 +0530

    contact page responsive

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -255,8 +255,8 @@ const Banner = () => {
           alt="Doctors"
           className="w-full h-full object-cover" // Ensure the image covers the div properly
         />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-white text-3xl md:text-4xl lg:text-5xl font-bold">
+        <div className="absolute  inset-0 flex items-center justify-center">
+          <h1 className="text-white text-3xl md:text-4xl lg:text-7xl font-bold">
             Contact Us.
           </h1>
         </div>
